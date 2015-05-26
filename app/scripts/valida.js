f8d4e99fa9ce993fa705dc81daea82d0ad05fae4
andres <andres_a4@hotmail.com>
2015-05-26 21:29:34 +0200

    Datos de facturacion

--- app/scripts/valida.js
+++ app/scripts/valida.js
@@ -17,26 +17,78 @@ $(document).ready(function() {
                 {
                 	required: true
                 },
-                tipodocumento:
+                email:
                 {
-                	required: true
+                    email: true,
+                    required: true,
+                    remote: 'http://localhost/juanda/proyecto_ajax/validar_email_db.php'
+                },
+                email2: 
+                {
+                    email: true,
+                    equalTo: '#email'
+                },
+                demandante:
+                {
+                    min: 1
+                },
+                dni:
+                {
+                    nifES: true,
+                    required: true,
+                    remote: 'http://localhost/juanda/proyecto_ajax/validar_nif_db.php'
+                },
+                direccion:
+                {
+                    required: true
+                },
+                CodPostal:
+                {
+                    required: true,
+                    remote: 'http://localhost/juanda/proyecto_ajax/validar_CodPostal_db.php'
                 }
             },
             messages: {
                 nombre:
                 {
                     required: 'Debes escribir tu nombre',
-//                    remote: 'Nombre no valido'
                 },
                 apellido1:
                 {
                     required: 'Debes escribir tu primer apellido',
-//                    remote: 'Nombre no valido'
                 },
                 apellido2:
                 {
                     required: 'Debes escribir tu segundo apellido',
-//                    remote: 'Nombre no valido'
+                },
+                email:
+                {
+                    email: 'Por favor escribe un email valido',
+                    required: 'Debes escribir tu e-mail',
+                    remote: 'El e-mail ya existe en la base de datos.'
+                },
+                email2: 
+                {
+                    email: 'Por favor escribe un email valido',
+                    equalTo: 'El e-mail no coincide'
+                },
+                demandante:
+                {
+                    min: 'Debes marcar una opcion'
+                },
+                dni:
+                {
+                    nifES: 'Debes escribir un NIF válido',
+                    required: 'Debes escribir tu CIF / NIF'
+                },
+                direccion:
+                {
+                	required: 'Debes escribir tu direccion'
+                },
+                CodPostal:
+                {
+                    required: 'Debes escribir tu Código Postal',
+                    remote: ''
                 }
             }
             
